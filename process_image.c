e3e59d0a95eeaa7fdb183984f9cc14d40aa7ae9a
Alex <alexandre.deterrassondemontleau@epfl.ch>
2021-04-19 17:09:54 +0200

    add camera prog

--- process_image.c
+++ process_image.c
@@ -13,13 +13,13 @@
 #include <process_image.h>
 
 
+#define EPSILON 10
+#define THRESHOLD 10
 static float distance_cm = 0;
 static uint16_t line_position = IMAGE_BUFFER_SIZE/2;	//middle
 
-
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
-static VL53L0X_DEV dev_chosen ;
 
 /*
  *  Returns the line's width extracted from the image buffer given
@@ -30,7 +30,6 @@ uint16_t extract_line_width(uint8_t *buffer){
 	uint16_t i = 0, begin = 0, end = 0, width = 0;
 	uint8_t stop = 0, wrong_line = 0, line_not_found = 0;
 	uint32_t mean = 0;
-
 	static uint16_t last_width = PXTOCM/GOAL_DISTANCE;
 
 	//performs an average
@@ -43,7 +42,7 @@ uint16_t extract_line_width(uint8_t *buffer){
 		wrong_line = 0;
 		//search for a begin
 		while(stop == 0 && i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE))
-		{ 
+		{
 			//the slope must at least be WIDTH_SLOPE wide and is compared
 		    //to the mean of the image
 		    if(buffer[i] > mean && buffer[i+WIDTH_SLOPE] < mean)
@@ -57,7 +56,7 @@ uint16_t extract_line_width(uint8_t *buffer){
 		if (i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE) && begin)
 		{
 		    stop = 0;
-		    
+
 		    while(stop == 0 && i < IMAGE_BUFFER_SIZE)
 		    {
 		        if(buffer[i] > mean && buffer[i-WIDTH_SLOPE] < mean)
@@ -105,6 +104,17 @@ uint16_t extract_line_width(uint8_t *buffer){
 	}
 }
 
+
+float get_distance_cm(void){
+	return distance_cm;
+}
+
+
+
+uint16_t get_line_position(void){
+	return line_position;
+}
+
 static THD_WORKING_AREA(waCaptureImage, 256);
 static THD_FUNCTION(CaptureImage, arg) {
 
@@ -128,7 +138,8 @@ static THD_FUNCTION(CaptureImage, arg) {
 }
 
 
-static THD_WORKING_AREA(waProcessImage, 1024);
+
+static THD_WORKING_AREA(waProcessImage, 1536);  //1024
 static THD_FUNCTION(ProcessImage, arg) {
 
     chRegSetThreadName(__FUNCTION__);
@@ -136,46 +147,348 @@ static THD_FUNCTION(ProcessImage, arg) {
 
 	uint8_t *img_buff_ptr;
 	uint8_t image[IMAGE_BUFFER_SIZE] = {0};
-	uint16_t lineWidth = 0;
+    uint8_t image_blue[IMAGE_BUFFER_SIZE] = {0};
+	//uint8_t image_red = 0;
+	//uint8_t image_blue = 0;
+	//uint8_t image_red[2*EPSILON] = {0};
+	//uint8_t image_blue[2*EPSILON] = {0};
 
-	bool send_to_computer = true;
+	//uint8_t image_blue_moy = 0;
+	//uint8_t image_red_moy = 0;
+	uint32_t image_blue_moy = 0;
+	uint32_t image_red_moy = 0;
+
+
+
+	uint16_t lineWidth = 0;
+	uint16_t center_line = IMAGE_BUFFER_SIZE/2;
+
+    //palClearPad(GPIOD, GPIOD_LED_FRONT); //if red
+	//palClearPad(GPIOB, GPIOB_LED_BODY); //if blue
+	//palClearPad(GPIOD, GPIOD_LED7); // if stranger
+
+
+    //bool friend_or_enemi = true; // true = friends //bleu = ami, rouge = emenie.
+    //bool total_stranger = false;
+
+
+	//bool send_to_computer = true;
 
     while(1){
+
     	//waits until an image has been captured
         chBSemWait(&image_ready_sem);
-		//gets the pointer to the array filled with the last image in RGB565    
+
+		//gets the pointer to the array filled with the last image in RGB565
 		img_buff_ptr = dcmi_get_last_image_ptr();
 
-		//Extracts only the red pixels
+
 		for(uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i+=2){
 			//extracts first 5bits of the first byte
 			//takes nothing from the second byte
-			image[i/2] = (uint8_t)img_buff_ptr[i]&0xF8;
+
+			image[i/2] = (((uint8_t)img_buff_ptr[i])>>3)&0x1F;  //va reperer bleu jsp pourquoi
+            //image_red_moy = image_red_moy + image[i/2];
+
+
+            image_blue[i/2] = ((uint8_t)img_buff_ptr[i+1])&0x1F;
+            //image_blue_moy = image_blue_moy + image_blue[i/2];
+
+
+
+
+
+			//image[i/2] = ((uint8_t)img_buff_ptr[i+1])&0x1F; //va reperer le rouge trop bizarre
+
+			//image_red_moy = image_red_moy + image[i/2];
+
+			}
+
+
+		for(uint16_t i = 2*(IMAGE_BUFFER_SIZE/2 - THRESHOLD) ; i < (2 * (IMAGE_BUFFER_SIZE/2 + THRESHOLD)) ; i+=2){
+
+			image_blue_moy = image_blue_moy + image_blue[i/2];
+			image_red_moy = image_red_moy + image[i/2];
 		}
 
-		//search for a line in the image and gets its width in pixels
-		lineWidth = extract_line_width(image);
 
-		//converts the width into a distance between the robot and the camera
-		if(lineWidth){
-			distance_cm = PXTOCM/lineWidth;
+		image_red_moy = image_red_moy/(2*THRESHOLD);
+		image_blue_moy = image_blue_moy/(2*THRESHOLD);
+
+		//ces 2 valeurs sont bcp trop proches
+
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[RED_MOY = %d]", image_red_moy);
+
+
+
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[BLUE_MOY = %d]", image_blue_moy);
+
+
+		if (image_red_moy > image_blue_moy) {
+
+			lineWidth = extract_line_width(image_blue);
+			if (lineWidth > 0) {
+
+				chprintf((BaseSequentialStream *)&SDU1, "[RED]");
+				right_motor_set_speed(1000);
+			    left_motor_set_speed(1000);
+
+
+
+			}
 		}
 
-		if(send_to_computer){
-			//sends to the computer the image
-			SendUint8ToComputer(image, IMAGE_BUFFER_SIZE);
+		if (image_blue_moy > image_red_moy) {
+
+			lineWidth = extract_line_width(image);
+			if (lineWidth > 0) {
+
+				chprintf((BaseSequentialStream *)&SDU1, "[BLUE]");
+				right_motor_set_speed(0);
+				left_motor_set_speed(0);
+			}
 		}
-		//invert the bool
-		send_to_computer = !send_to_computer;
-    }
+
+		//else {
+
+			//chprintf((BaseSequentialStream *)&SDU1, "NOLINE");
+		//}
+
+
+		//lineWidth = extract_line_width(image);
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[RED_MOY = %d]", image_red_moy);
+
+		//for(uint32_t i = 0 ; i < 21000000 ; i++){
+								// __asm__ volatile ("nop");
+						//}
+
+
+		//center_line = line_position;
+
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[center_line = %d]", center_line);
+
+		//if(lineWidth > 0 && center_line > 0) {
+
+			//chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_red_moy);
+
+
+
+		//}
+
+		//else {
+
+			//chprintf((BaseSequentialStream *)&SDU1, "NOP" );
+
+		//}
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[CENTER_LINE = %d]", center_line);
+
+		//chprintf((BaseSequentialStream *)&SDU1, "[lineWidth = %d]", lineWidth);
+
+		//image_red = ((uint8_t)img_buff_ptr[i])&0xF8;
+		//image_blue = (uint8_t)img_buff_ptr[i+1]&0X1F
+
+
+		//if(send_to_computer){
+			    			//sends to the computer the image
+			    	//	SendUint8ToComputer(image, IMAGE_BUFFER_SIZE);
+			    		//}
+
+			 //  send_to_computer = !send_to_computer;
+
+
+		//if(lineWidth) {
+
+/*
+
+			for(uint16_t i = 0 ; i < (4*EPSILON) ; i+=2){  //for(uint16_t i = 2*(center_line - EPSILON) ; i < (2*(center_line + EPSILON)) ; i+=2){
+
+				image_red[i/2] =  (((uint8_t)img_buff_ptr[2*(center_line - EPSILON) + i])>>3)&0x1F; //normalement :image_red[i/2] =  ((uint8_t)img_buff_ptr[i]>>3)&0x1F;
+
+				image_blue[i/2] = (uint8_t)img_buff_ptr[2*(center_line - EPSILON) + i + 1]&0X1F;  //normalement :image_blue[i/2] =  ((uint8_t)img_buff_ptr[i+1)&0x1F;
+
+				image_blue_moy = image_blue_moy + image_blue[i/2];
+				image_red_moy = image_red_moy + image_red[i/2];
+
+			}
+
+			image_red_moy = image_red_moy/(2*EPSILON);
+			image_blue_moy = image_blue_moy/(2*EPSILON);
+
+
+
+			if ( image_red_moy < image_moy ) { //blue repere
+
+
+				chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_blue_moy);
+
+			}
+
+			if ( image_blue_moy < image_moy ) { //red repere
+
+				chprintf((BaseSequentialStream *)&SDU1, "[RED = %d]", image_blue_moy);
+			}
+
+			else {
+				chprintf((BaseSequentialStream *)&SDU1, "[NOP]");
+
+			}
+
+
+
+			//for(uint32_t i = 0 ; i < 21000000 ; i++){
+									// __asm__ volatile ("nop");
+							//}
+
+			//chprintf((BaseSequentialStream *)&SDU1, "[RED = %d]", image_red_moy);
+
+			//for(uint32_t i = 0 ; i < 21000000 ; i++){
+									// __asm__ volatile ("nop");
+							//}
+
+
+			//chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_blue_moy);
+
+
+		//}
+
+
+
+*/
+
+
+/*
+
+
+			if (image_red_moy > image_blue_moy) {
+
+
+
+				chprintf((BaseSequentialStream *)&SDU1, "[RED = %d]", image_red_moy);
+
+				//right_motor_set_speed(1000);
+				//left_motor_set_speed(1000);
+
+				for(uint32_t i = 0 ; i < 21000000 ; i++){
+						 __asm__ volatile ("nop");
+				}
+
+				}
+
+			if(image_blue_moy > image_red_moy ) {
+
+				chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_blue_moy);
+
+
+				//right_motor_set_speed(0);
+				//left_motor_set_speed(0);
+
+				for(uint32_t i = 0 ; i < 21000000 ; i++){
+							__asm__ volatile ("nop");
+				}
+
+			}
+
+			else {
+
+				//right_motor_set_speed(0);
+			    //left_motor_set_speed(0);
+
+				//total_stranger = true;
+				chprintf((BaseSequentialStream *)&SDU1, "NOP");
+
+			    for(uint32_t i = 0 ; i < 21000000 ; i++){
+						    __asm__ volatile ("nop");
+							}
+
 }
 
-float get_distance_cm(void){
-	return distance_cm;
-}
 
-uint16_t get_line_position(void){
-	return line_position ;
+
+*/
+
+/*
+commentaire: peut etre mon erreur est que je compare un registre pour 1 pixel et pas plusieurs !
+			//center_line = get_line_position();
+
+			image_red = (((uint8_t)img_buff_ptr[2*center_line])>>3)&0x1F;
+			image_blue = ((uint8_t)img_buff_ptr[2*center_line+1])&0X1F;
+
+			//chprintf((BaseSequentialStream *)&SDU1, "[RED = %d]", image_red);
+
+			//chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_blue);
+
+
+
+
+
+
+
+		if (image_red - image_blue > EPSILON/2) {
+
+			chprintf((BaseSequentialStream *)&SDU1, "[RED = %d]", image_red);
+
+			right_motor_set_speed(1000);
+			left_motor_set_speed(1000);
+
+			for(uint32_t i = 0 ; i < 21000000 ; i++){
+					 __asm__ volatile ("nop");
+			}
+
+
+			//friend_or_enemi = false;
+			//lineWidth = extract_line_width(image_red);
+			//chprintf((BaseSequentialStream *)&SDU1, "RED");
+			//palTogglePad(GPIOD, GPIOD_LED_FRONT);
+
+			}
+
+		if(image_blue - image_red > EPSILON) {
+
+			chprintf((BaseSequentialStream *)&SDU1, "[BLUE = %d]", image_blue);
+
+
+			right_motor_set_speed(0);
+			left_motor_set_speed(0);
+
+			for(uint32_t i = 0 ; i < 21000000 ; i++){
+						__asm__ volatile ("nop");
+			}
+			//friend_or_enemi = true;
+		    //lineWidth = extract_line_width(image_blue);
+			//palTogglePad(GPIOB, GPIOB_LED_BODY); //if blue
+		    //chprintf((BaseSequentialStream *)&SDU1, "BLUE");
+
+
+		}
+
+		else {
+
+			right_motor_set_speed(0);
+		    left_motor_set_speed(0);
+
+			//total_stranger = true;
+			chprintf((BaseSequentialStream *)&SDU1, "NOP");
+
+			//for(uint32_t i = 0 ; i < 21000000 ; i++){
+					     //__asm__ volatile ("nop");
+						//}
+
+		}
+
+		//for(uint32_t i = 0 ; i < 21000000 ; i++){
+								     // __asm__ volatile ("nop");
+								//}
+*/
+
+
+
+  }
+
 }
 
 void process_image_start(void){
